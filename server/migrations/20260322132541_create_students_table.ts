5c945d5d7dd84d250d55d1299730d76e72c2bf55
Hristo Atanasov <hristo@ico.ico.org>
2026-04-05 16:01:47 +0300

    initial db tables ico

--- server/migrations/20260322132541_create_students_table.ts
+++ server/migrations/20260322132541_create_students_table.ts
@@ -2,9 +2,30 @@ import type { Knex } from "knex";
 
 
 export async function up(knex: Knex): Promise<void> {
+    await knex.schema.createTableIfNotExists('students', (table) => {
+        table.string('faculty_number').primary().index();
+        table.string('ucn').unique().notNullable();
+        table.enum('financing', ['държавна поръчка', 'платено обучение']).notNullable();
+        table.string('adress').notNullable();
+        table
+            .integer('user_id')
+            .notNullable()
+            .references('users')
+            .unique()
+            .onDelete('CASCADE')
+            .onUpdate('CASCADE');
+        table
+            .integer('curriculum_id')
+            .notNullable()
+            .references('curriculums')
+            .onDelete('CASCADE')
+            .onUpdate('CASCADE');
+        table.timestamps(true, true);
+    })
 }
 
 
 export async function down(knex: Knex): Promise<void> {
+    await knex.schema.dropTableIfExists('students');
 }
 
---
+++ server/migrations/20260405123406_create_curriculums_courses_table.ts
@@ -0,0 +1,29 @@
+import type { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+    await knex.schema.createTableIfNotExists('curriculums_courses', (table) => {
+        table.increments('id').primary();
+        table
+            .integer('curriculum_id')
+            .notNullable()
+            .references('curriculums')
+            .onDelete('CASCADE')
+            .onUpdate('CASCADE');
+        table
+            .integer('course_id')
+            .notNullable()
+            .references('courses')
+            .onDelete('CASCADE')
+            .onUpdate('CASCADE');
+        table.integer('credits').notNullable();
+        table.timestamps(true, true);
+        table.unique(['curriculum_id', 'course_id']);
+    })
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+    await knex.schema.dropTableIfExists('curriculums_courses');
+}
+
